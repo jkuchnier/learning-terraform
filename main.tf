748de0920032fbcdbf8404e63aa193a28174bd96
João Kuchnier <82408092+jkuchnier@users.noreply.github.com>
2024-02-27 12:35:50 -0300

    One more try

--- main.tf
+++ main.tf
@@ -71,12 +71,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id        = aws_instance.blog.id
     }
   }
 
