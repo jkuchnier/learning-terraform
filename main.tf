7c5e473d418b9dca23d95ca0d30d06a09052f273
João Kuchnier <82408092+jkuchnier@users.noreply.github.com>
2024-02-27 12:03:27 -0300

    More fixes

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws.instance.blog.id
+          target_id = aws_instance.blog.id
           port = 80
         }
       }
