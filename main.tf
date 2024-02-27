e8534f5c135b95da747eaf69591c382cb8606366
João Kuchnier <82408092+jkuchnier@users.noreply.github.com>
2024-02-27 11:59:53 -0300

    Load balancer

--- main.tf
+++ main.tf
@@ -47,6 +47,43 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_vpc.security_group_id
+  
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        target_id = aws.instance.blog.id
+        port = 80
+      }
+    }
+  ]
+  
+  http_tcp_listeners = [
+    {
+      port                = 80
+      protocol            = "HTTP"
+      target_group_indes  = 0
+    }
+  ]
+
+  tags = {
+      Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
